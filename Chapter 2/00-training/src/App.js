25bc49c8e5314155c0a52436637a4878b18fccbf
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-09-04 04:41:55 -0500

    adding props to components

--- Chapter 2/00-training/src/App.js
+++ Chapter 2/00-training/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UseStateToDelete from './Components/UseStateToDelete/UseStateToDelete'
 import Booleann from './Components/UseStateAsBoolean/Boolean'
 import Props from './Components/Props/Props';
+import Modal from './Components/Modal/Modal';
 
 function App() {
   const title = 'Hello There'
@@ -14,9 +15,13 @@ function App() {
         <Route path="/" element={<UseStateToDelete/>} />
         <Route path='/boolean' element={<Booleann />} />
       </Routes>
-      
+
       {/* using props exmpl */}
       <Props title={title} name='jugurta'></Props>
+      <Modal>
+        <h2>10% off coupon code</h2>
+        <p>Use the code Ninja jay</p>
+      </Modal>
     </div>
     </Router>
   );
